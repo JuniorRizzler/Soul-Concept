846d53f04e816e12b6973c883f54e233799bc040
Dean Concepcion <2029089@appleby.on.ca>
2026-03-07 11:50:21 -0500

    Replace Grade 9 science library with new TSX and rebuild study bundle

---
+++ _science_study_library_entry.tsx
@@ -0,0 +1,8 @@
+﻿import React from 'react';
+import { createRoot } from 'react-dom/client';
+import ScienceStudyLibrary from './math/ScienceStudyLibrary';
+
+const rootEl = document.getElementById('root');
+if (rootEl) {
+  createRoot(rootEl).render(<ScienceStudyLibrary />);
+}
